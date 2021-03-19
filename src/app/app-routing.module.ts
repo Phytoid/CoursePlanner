8c88a400408f2ba87af37a7c50b81471827b838a
Eric Wang <e.wang7777@gmail.com>
2021-03-19 15:59:59 -0400

    added gpd component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { LoginComponent } from './components/login/login.component';
 import { StudentComponent } from './components/student/student.component';
+import {GpdComponent} from './components/gpd/gpd.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'student', component: StudentComponent }
+  { path: 'student', component: StudentComponent },
+  { path: 'gpd', component: GpdComponent }
 ];
 
 @NgModule({
