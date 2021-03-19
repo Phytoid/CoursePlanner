c1016c3d909ca5b3e8403b434c167ac337c72e65
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-03-19 17:14:54 -0400

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+import { Observable } from 'rxjs';
+
+interface User {
+  email:string;
+  password:string;
+}
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+
+export class LoginComponent implements OnInit {
+  
+  //constructor() { }
+  constructor(private db: AngularFireDatabase) {}
+
 
   ngOnInit(): void {
+    
     console.log("init login")
+    this.db.database.ref().child("Persons").on('value', (shapshot) => {
+      shapshot.forEach((child) => {
+         
+          console.log(child.val())
+      })
+    })
   }
+    
+
+    
+  
 
   loginUser(event){
     console.log(event)
