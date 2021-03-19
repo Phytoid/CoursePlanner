c4590a95971fc2cbf9473e99065042583bda1004
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-03-19 16:18:00 -0400

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCglbveaq3v8oMV_p7XGpJI4d6cGHI8p08",
+    authDomain: "mast-b4804.firebaseapp.com",
+    databaseURL: "https://mast-b4804-default-rtdb.firebaseio.com",
+    projectId: "mast-b4804",
+    storageBucket: "mast-b4804.appspot.com",
+    messagingSenderId: "1065825720921",
+    appId: "1:1065825720921:web:481f562fbd98d31f796f48",
+    measurementId: "G-NMR870P1FW"
+  
+  }
 };
 
 /*
