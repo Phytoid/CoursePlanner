3930a027f4e8d690640625aca4ebd6fe1dbaabe6
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-03-19 19:01:26 -0400

    Login -> Student page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 import { Observable } from 'rxjs';
+import { RouterModule, Routes } from '@angular/router';
+
+import { Router } from '@angular/router';
 
 interface User {
   email:string;
@@ -21,7 +24,7 @@ interface User {
 export class LoginComponent implements OnInit {
   
   //constructor() { }
-  constructor(private db: AngularFireDatabase) {}
+  constructor(private db: AngularFireDatabase, private router: Router) {}
 
 
   ngOnInit(): void {
@@ -35,24 +38,25 @@ export class LoginComponent implements OnInit {
   
 
   loginUser(event){
-    console.log(event)
+    console.log("Calling login!")
     let email = event.srcElement[0].value;
     let password = event.srcElement[1].value;
-    this.db.database.ref().child("Persons").on('value', (shapshot) => {
-      shapshot.forEach((child) => {
+    this.db.database.ref().child("Persons").on('value', (snapshot) => { // TODO: cant exit for each loop in angular : https://github.com/angular/angular.js/issues/263
+      snapshot.forEach((child) => {
           var data = child.val();
           let email2 = data[Object.keys(data)[0]].email
           if(email == email2) {
             if(password == data[Object.keys(data)[0]].password) {
               console.log("Success!")
-              return;
+              this.router.navigateByUrl("/student");
+              return
             }
           }
 
           
       })
     })
-    console.log("Failed to login!")
+    console.log("Failed to login!") // This will run even if log in succesful
     event.preventDefault()
   }
 
