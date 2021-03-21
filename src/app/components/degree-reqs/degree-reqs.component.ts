34e731377cd49aada51b5c4294fce0045065c68b
Eric Wang <e.wang7777@gmail.com>
2021-03-21 15:16:18 -0400

    degree requirements page

---
+++ src/app/components/degree-reqs/degree-reqs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-degree-reqs',
+  templateUrl: './degree-reqs.component.html',
+  styleUrls: ['./degree-reqs.component.css']
+})
+export class DegreeReqsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
