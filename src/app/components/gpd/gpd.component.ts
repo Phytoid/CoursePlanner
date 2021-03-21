5bc96e8aae8fafb6694422cc9a950ea78975f421
Justyn Ngo <justyn.ngo@stonybrook.edu>
2021-03-21 13:55:52 -0400

    Added delete functionality for GPD

--- src/app/components/gpd/gpd.component.ts
+++ src/app/components/gpd/gpd.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-gpd',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GpdComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
   }
 
+  onDelete(){
+    if(confirm("Are you sure you want to delete all students?")){
+      this.db.database.ref().child("Persons").child("Students").on('value', (shapshot) => {
+        shapshot.forEach((child) => {
+           
+            //console.log(child.val())
+            if(child.ref.remove()){
+              console.log("Delete");
+            };
+        })
+      })
+    }
+  }
 }
