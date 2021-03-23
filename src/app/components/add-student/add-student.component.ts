345c78261e73bad0cc9077fe29766d6cf8a310ef
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-03-23 10:31:55 -0400

    add student

--- src/app/components/add-student/add-student.component.ts
+++ src/app/components/add-student/add-student.component.ts
@@ -3,6 +3,10 @@ import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import { Student } from '../../models/student';
 
 
 @Component({
@@ -10,19 +14,34 @@ import { first } from 'rxjs/operators';
   templateUrl: './add-student.component.html',
   styleUrls: ['./add-student.component.css']
 })
+
+
+
+
 export class AddStudentComponent implements OnInit {
   registerForm: FormGroup;
   loading = false;
   submitted = false;
   error: string;
-  
-  constructor(private authService: AuthService, public router: Router) {
+  s:Student;
+
+  constructor(private authService: AuthService, public router: Router, public afs: AngularFirestore) {
     if (this.authService.isLoggedIn == false) {
       this.router.navigate(['login'])
     }
   }
 
   ngOnInit(): void {
+  }
+
+  addStudent(event) {
+    console.log("Adding Student!")
+    let first_name = event.srcElement[0].value
+    this.s = {
+      first: 'A',
+      last: 'B'
+    }
+    this.afs.firestore.collection('Students').add(this.s);
   }
 
 }
