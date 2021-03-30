74a7181419c0f2827fdd5ae2a4742be1c329493b
Eric Wang <e.wang7777@gmail.com>
2021-03-30 17:56:15 -0400

    edited courses

--- src/app/models/courses.ts
+++ src/app/models/courses.ts
@@ -1,14 +1,14 @@
 import { Semester } from "./semester.enum";
 
 export interface Courses {
-    course_identifier?:String;
-    course_name?:String;
+    courseID?:String;
+    courseName?:String;
     professor?:String;
     semester?:Semester;
     year?:Number;
-    start_time?:Number;
-    end_time?:Number;
-    graduate_prereqs?:String;
+    startTime?:Date;
+    endTime?:Date;
+    graduatePreq?:String;
     enrollment?:Number;
     section?:Number;
     credits?:Number;
