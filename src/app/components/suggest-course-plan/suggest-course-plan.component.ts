5d1552f66c44cb067e586f17ededc4bee04ffff6
Eric Wang <e.wang7777@gmail.com>
2021-04-02 14:02:11 -0400

    edited course plan page

--- src/app/components/suggest-course-plan/suggest-course-plan.component.ts
+++ src/app/components/suggest-course-plan/suggest-course-plan.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggest-course-plan.component.css']
 })
 export class SuggestCoursePlanComponent implements OnInit {
-
+  searchColumns: string[] = ['sbuID', 'lastName', 'firstName', 'dept', 
+  'track', 'coursePlan', 'satisfied', 'pending', 'unsatisfied', 
+  'validCoursePlan', 'gradSemester', 'gradYear', 'semesters', 'graduated']
   constructor() { }
 
   ngOnInit(): void {
