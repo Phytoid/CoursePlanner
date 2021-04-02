6aa5dfd1a1d32c139329554e0a045b9474228faa
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-04-02 02:45:02 -0400

    file checks for import student data

--- src/app/components/gpd/gpd.component.ts
+++ src/app/components/gpd/gpd.component.ts
@@ -54,12 +54,35 @@ export class GpdComponent implements OnInit {
     let course_data_header = "sbu_id,department,course_num,section,semester,year,grade";
     let student_data_header = "sbu_id,first_name,last_name,email,department,track,entry_semester,entry_year,requirement_version_semester,requirement_version_year,graduation_semester,graduation_year,password";
 
+    let text1 = await (await fileList.item(0).text()).split(/\r?\n/)[0]; // sbu_id,first_name,last_name,email,department,track,entry_semester,entry_year,requirement_version_semester,requirement_version_year,graduation_semester,graduation_year,password
+    let text2 = await (await fileList.item(1).text()).split(/\r?\n/)[0]; // sbu_id,department,course_num,section,semester,year,grade1
+    
+    var course_data = 0;
+    var student_data = 0;
 
+    if(text1 == course_data_header) {
+      course_data++;
+    } else if(text1 == student_data_header) {
+      student_data++;
+    } 
 
-    let text1 = await fileList.item(0).text(); // sbu_id,first_name,last_name,email,department,track,entry_semester,entry_year,requirement_version_semester,requirement_version_year,graduation_semester,graduation_year,password
-    let text2 = await fileList.item(1).text(); // sbu_id,department,course_num,section,semester,year,grade
-    console.log(text1);
-    console.log(text2);
+    if(text2 == course_data_header) {
+      course_data++;
+    } else if(text2 == student_data_header) {
+      student_data++;
+    } 
+
+    console.log(course_data);
+    console.log(student_data);
+
+    if(course_data > 1 || student_data > 1) {
+      alert("Both files are of the same type.")
+      return;
+    } else if (course_data < 1 || student_data < 1) {
+      alert("One or more files were not of the right format");
+      return;
+    }
+
 
   }
 
