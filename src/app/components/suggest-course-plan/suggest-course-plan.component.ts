9c2712e4d06a567526c525c4525318e16d948252
Eric Wang <e.wang7777@gmail.com>
2021-04-25 16:29:08 -0400

    worked on suggestcourseplan page

--- src/app/components/suggest-course-plan/suggest-course-plan.component.ts
+++ src/app/components/suggest-course-plan/suggest-course-plan.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Courses } from 'src/app/models/courses';
+import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-suggest-course-plan',
@@ -11,8 +13,12 @@ export class SuggestCoursePlanComponent implements OnInit {
   prefWeight: number = 0;
   courseAdd: string[] = ['add', "weight"];
   courseAvoid: string[] = ['avoid'];
+  c: string[] = ["a","b","c","d","e","f","g","h","i","j","k","l"];
   @ViewChild(MatSort) sort: MatSort;
-  constructor() { }
+  courses: Courses[];
+  coursesCopy: Courses[];
+  constructor(public courseServices:CourseService) { 
+  }
   coursesToAddList=[
     {
       "courseName": "CSE 101", "prefWeight": this.prefWeight
@@ -24,14 +30,25 @@ export class SuggestCoursePlanComponent implements OnInit {
     }
   ]
   ngOnInit(): void {
-
+    // let arr = [];
+    // this.courseServices.getCourses().subscribe(s => {
+    //   s.forEach(element => {
+    //       arr.push(element);
+    //   });
+    // });
+    // this.courses = arr;
+    // this.coursesCopy = arr;
+  }
+  ngAfterInit(): void{
+    location.reload();
   }
   addItemPref(): void {
     this.prefWeight++;
-    this.coursesToAddList.push({courseName:"CSE 110", "prefWeight": this.prefWeight});
+    this.coursesToAddList = this.coursesToAddList.concat({courseName:"CSE 110", "prefWeight": this.prefWeight});
     console.log(this.coursesToAddList);
   }
   addItemAvoid(): void{
-    this.coursesToAvoidList.push();
+    this.coursesToAvoidList = this.coursesToAvoidList.concat();
+    console.log(this.coursesToAvoidList);
   }
 }
