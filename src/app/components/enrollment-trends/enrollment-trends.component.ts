90f491d3b4a80f0746a337ee1a087e6fa657a21e
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2021-04-27 14:59:09 -0400

    basics of enrollment trends graph

--- src/app/components/enrollment-trends/enrollment-trends.component.ts
+++ src/app/components/enrollment-trends/enrollment-trends.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { StudentService } from 'src/app/services/student.service';
 
 
+
 @Component ({
   selector: 'app-enrollment-trends',
   templateUrl: './enrollment-trends.component.html',
@@ -14,16 +15,19 @@ import { StudentService } from 'src/app/services/student.service';
 })
 
 export class EnrollmentTrendsComponent implements OnInit {
+  
+
   display: boolean = false;
 
   courses_to_display = [];
+  semesters_to_display = [];
   
 
   arr: any = [];
-  public graph = { data: [
-      { x: [], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} }
-    ],
-    layout: {width: 320, height: 240, title: 'Course Enrollment Trends'}
+  public graph =  { data: [
+          
+  ],
+  layout: {width: 640, height: 480, title: 'Course Enrollment Trends'}
   };
   constructor(private authService: AuthService, public router: Router, public afs: AngularFirestore, public studentService: StudentService) {
     if (!this.authService.isLoggedIn) {
@@ -41,55 +45,122 @@ export class EnrollmentTrendsComponent implements OnInit {
     });
   }
 
-
-
+  
+  
 
   addCourse(event) {
-    console.log("add course!\n");
-    this.display = true;
+    
+    this.semesters_to_display = [];
 
-    //this.graph = {
-    //  data: [
-    //     { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
-    //      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
-    // ],
-    //  layout: {width: 320, height: 240, title: 'A Fancy Plot'}
-    //};
+    console.log("add course!\n");
+    
+
+    
     let department = ((<HTMLInputElement>document.getElementById("mat-input-0")).value);
     let course = ((<HTMLInputElement>document.getElementById("mat-input-1")).value);
 
     let semester_start = ((<HTMLInputElement>document.getElementById("mat-input-2")).value);
     let semester_end = ((<HTMLInputElement>document.getElementById("mat-input-3")).value);
 
-    let year_start = ((<HTMLInputElement>document.getElementById("mat-input-4")).value);
-    let year_end = ((<HTMLInputElement>document.getElementById("mat-input-5")).value);
+  
+    let year_start = parseInt(((<HTMLInputElement>document.getElementById("mat-input-4")).value));
+    let year_end = parseInt(((<HTMLInputElement>document.getElementById("mat-input-5")).value));
 
-   // console.log(semester_start + " " + year_start);
-   // console.log(semester_end + " " + year_end);
+    // Get semesters to display
 
-   this.courses_to_display.push(course);
-
-   var amount_of_class = 0;
-
-   this.graph =  { data: [
-        { x: this.courses_to_display, y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} }
-      ],
-      layout: {width: 320, height: 240, title: 'Course Enrollment Trends'}
-     };
     
-    this.arr.forEach(student => {
+    
+      
+
+    
+    var year = year_start;
+    var semester = semester_start;
+    while(true) {
+      
+      if(year > year_end) {
+        break;
+      } else if(year == year && (semester == "fall" && semester_end == "spring" )) {
+        break;
+      }
+      console.log(semester + " " + year);
+      this.semesters_to_display.push(semester + " " + year);
+      if(semester == "spring") {
+        semester = "fall";
+      } else if(semester == "fall") {
+        semester = "spring";
+        year = year + 1;
+      }
+      
+      if(this.semesters_to_display.includes(semester + " " + year))
+        continue;
+     
+      
+
+    }
+
+   
+    console.log(this.semesters_to_display);
+    
+
+    this.courses_to_display.push(course);
+
+    var amount_of_class = 0;
+
+    var course_enrollment = new Array(this.semesters_to_display.length).fill(0);
+
+    var plot_data = {
+      x: this.semesters_to_display,
+      y: course_enrollment,
+      type: 'scatter',
+      mode:'lines',
+      name: course,
+      marker: {color: 'red'},
+      hoverinfo:'x+y+name',
+    };
+
+
+
+    //this.graph =  { data: [
+    //      { x: this.semesters_to_display, y: course_enrollment, type: 'scatter', mode: 'lines+points', marker: {color: 'red'} }
+    //    ],
+    //    layout: {width: 640, height: 480, title: 'Course Enrollment Trends'}
+    //};
+
+   
+
+    
+    
+
+    console.log(course_enrollment);
+    
+
+    this.arr.forEach(student => { // loop through each student
+      
       let plan = student.coursePlan;
       if(plan != undefined) {
         var i = 0;
-        for(var sem in plan) {
+        for(var sem in plan) { // go through each semester in course plan
+         
+          
           let split = sem.toString().toLowerCase().split(/(\d+)/)
           let season = split[0];
-          let year = split[1];
-         //console.log(season); // text
-         // console.log(year); // year
+          let year = parseInt(split[1]);
+          
+          var index_to_add = this.semesters_to_display.indexOf(season + " " + year);
+          if(index_to_add < 0)
+            continue;
+          
+          if(year == year_end)
+          {
+            if(semester_end == "spring" && season == "fall")
+                continue;
+            if(semester_start == "fall" && season == "spring")
+                continue;
+            
+          }
 
           if(year <= year_end && year >= year_start) {
-           
+          
             var classes_for_semester = plan[Object.keys(plan)[i]];
             for(var student_class in classes_for_semester) {
               let split = student_class.toString().toLowerCase().split(/(\d+)/)
@@ -97,6 +168,8 @@ export class EnrollmentTrendsComponent implements OnInit {
               let dept_class_num = split[0] + " " + split[1];
             
               if(dept_class_num == course) { 
+                console.log(student.id);
+                course_enrollment[index_to_add]++;
                 amount_of_class++;
               }
             }
@@ -109,10 +182,18 @@ export class EnrollmentTrendsComponent implements OnInit {
       }
       
     });
-   
+    this.graph.data.push(plot_data);
+    console.log(course_enrollment);
     console.log(amount_of_class);
     
     
   }
+  
+  viewChart(event) {
+    console.log("add course!\n");
+    this.display = true;
+  }
 
 }
+
+
